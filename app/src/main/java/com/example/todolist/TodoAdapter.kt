9199d3ce8b0d6cbde4373c59bbf0561f5d5313d7
George Salib <salibgeorge91372@gmail.com>
2021-08-17 19:33:47 +0200

    added TodoAdapter

--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.todolist
 
-class TodoAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
+
+class TodoAdapter(
+        private val todos: MutableList<Todo>
+): RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+    class TodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                        R.layout.item_todo,
+                        parent,
+                        false
+                )
+        )
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val curTodo = todos[position]
+        holder.itemView.tvTodoTitle.text = curTodo.title
+        holder.itemView.cbDone.isChecked = curTodo.isChecked
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
 }
